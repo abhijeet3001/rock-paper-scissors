c074f2ad7160b4ab4a5978eb215ea048d9ae3e93
Abhijeet Singh <89379140+abhijeet3001@users.noreply.github.com>
2023-02-18 23:53:58 +0530

    Add playRound: Play single round

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function getComputerChoice() {
   let val = Math.floor(Math.random() * 3) + 1;
-  if (val === 1) return "Rock";
-  if (val === 2) return "Paper";
-  if (val === 3) return "Scissor";
+  if (val === 1) return "rock";
+  if (val === 2) return "paper";
+  if (val === 3) return "scissors";
+  
+}
+function playRound(playerSelection, computerSelection) {
+  playerSelection=playerSelection.toLowerCase();
+  if(playerSelection===computerSelection)
+    return "Tie";  
+  if (playerSelection === "rock") {
+    return computerSelection === "paper"? "You Lose! Paper beats Rock": "You Win! Rock beats Scissors";
+  }
+  if (playerSelection === "paper") {
+    return computerSelection === "scissors"? "You Lose! Scissors beats Paper": "You Win! Paper beats Rock";
+  }
+  if (playerSelection === "scissors") {
+    return computerSelection === "rock"? "You Lose! Rock beats Scissors": "You Win! Scissors beats Paper";
+  }
 }
