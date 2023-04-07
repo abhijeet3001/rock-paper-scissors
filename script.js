c6ce426c4d3e3da4cee61567cc36bbd16bce4fa9
Abhijeet Singh <89379140+abhijeet3001@users.noreply.github.com>
2023-04-07 07:51:53 +0530

    Make game work for 5 rounds and display result

--- script.js
+++ script.js
@@ -1,29 +1,65 @@
+let playerScore = 0;
+let computerScore = 0;
+let isGameFinished=false;
+
 function getComputerChoice() {
   let val = Math.floor(Math.random() * 3) + 1;
   if (val === 1) return "rock";
   if (val === 2) return "paper";
-  if (val === 3) return "scissors";
-  
+  if (val === 3) return "scissors";I
 }
 function playRound(playerSelection, computerSelection) {
-  playerSelection=playerSelection.toLowerCase();
-  if(playerSelection===computerSelection)
-    return "Tie";  
+  if(isGameFinished) return ;
+  if (playerSelection === computerSelection) return "Tie";
   if (playerSelection === "rock") {
-    return computerSelection === "paper"? "You Lose! Paper beats Rock": "You Win! Rock beats Scissors";
+    if (computerSelection === "paper") {
+      scores("computer");
+      return "You Lose! Paper beats Rock";
+    } else {
+      scores("player");
+      return "You Win! Rock beats Scissors";
+    }
   }
   if (playerSelection === "paper") {
-    return computerSelection === "scissors"? "You Lose! Scissors beats Paper": "You Win! Paper beats Rock";
+    if (computerSelection === "scissors") {
+      scores("computer");
+      return "You Lose! Scissors beats Paper";
+    } else {
+      scores("player");
+      return "You Win! Paper beats Rock";
+    }
   }
   if (playerSelection === "scissors") {
-    return computerSelection === "rock"? "You Lose! Rock beats Scissors": "You Win! Scissors beats Paper";
+    if (computerSelection === "rock") {
+      scores("computer");
+      return "You Lose! Rock beats Scissors";
+    } else {
+      scores("player");
+      return "You Win! Scissors beats Paper";
+    }
   }
 }
-const resultbox=document.querySelector('.result');
 
-function game(e){
-  let result=playRound(e.target.value,getComputerChoice());
-  resultbox.textContent=result;
+
+const resultbox = document.querySelector(".result");
+
+function scores(winner) {
+  winner === "player" ? playerScore++ : computerScore++;
+  const playerScoreboard = document.querySelector(".player");
+  const computerScoreboard = document.querySelector(".computer");
+  playerScoreboard.textContent = playerScore;
+  computerScoreboard.textContent = computerScore;
+  if (playerScore === 5 || computerScore === 5) {
+    isGameFinished=true;
+  }
 }
-const btn=document.querySelectorAll('.btn');
-btn.forEach(button => button.addEventListener('click',game));
+
+function game(e) {
+  let result = playRound(e.target.value, getComputerChoice());
+  resultbox.textContent = result;
+  if(isGameFinished)
+  resultbox.textContent =
+    playerScore === 5 ? "You won the game" : "You lost the game to computer";
+}
+const btn = document.querySelectorAll(".btn");
+btn.forEach((button) => button.addEventListener("click", game));
